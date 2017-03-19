e512ebec9c1a1016a583da1a9487f3f5cf5ee0b7
prajwal <prajwal111nayak@gmail.com>
2017-03-19 21:55:29 +0530

    responsive app with webpack

---
+++ src/app/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Header=(props)=> {
+    return(
+            <nav className="jumbotron">
+                <div className="well well-sm">
+                           <h1>"ReactJs-Assignment"</h1>
+                 </div>  
+            </nav>
+        );
+   };
---
+++ src/app/components/Home.js
@@ -0,0 +1,103 @@
+import React from "react";
+
+export class Home extends React.Component{
+ 
+    render(){
+        
+        return(
+            <div>
+                <div className="container-fluid">
+                    <div className="row">
+                        <div className="col-lg-3 col-xs-12 col-md-6 alert-success">
+                            <div className="intro">
+                            <div className=" col-lg-12 alert-info">
+                                <h2>HTML</h2>
+                            </div>
+                             
+                            
+                                <p>HTML is a markup language that is used for creating web pages. The HTML tutorial section will help you understand the basics of HTML, so that you can create your own web pages.</p>
+                            </div>
+                        </div>
+                           <div  className="col-lg-3 col-xs-12  col-md-6 alert-success" >
+                            <div className="intro">
+                            <div className=" col-lg-12 alert-info">
+                                    <h2>CSS</h2>
+                            </div>
+                          
+                                <p>CSS is used for describing the presentation of web pages. The CSS tutorial section will help you learn the essentials of CSS, so that you can fine control the style and layout of your HTML document.</p>
+                           </div>
+                        </div>
+                        <div className=" col-lg-3 col-xs-12 col-md-6 alert-success">
+                            <div className="intro">
+                            <div className=" col-lg-12 alert-info">
+                                <h2>Bootstrap</h2>
+                            </div>
+                                                   
+                                <hr/>
+            
+                                <p>Bootstrap is a powerful front-end framework for faster and easier web development. The Bootstrap tutorial section will help you learn the techniques of Bootstrap so that you can create web your own website.</p>
+                            </div>
+                        </div>
+                        <div className=" col-lg-3 col-xs-12 col-md-6 alert-success">
+                            <div className="intro">
+                            <div className=" col-lg-12 alert-info">
+                                <h2>Jquery</h2>
+                            </div>
+                                                 
+                                <hr/>
+            
+                                <p>Bootstrap is a powerful front-end framework for faster and easier web development. The Bootstrap tutorial section will help you learn the techniques of Bootstrap so that you can create web your own website.</p>
+                          </div>
+                        </div>
+                    </div>
+                     <div className="row">
+                        <div className="col-lg-3 col-xs-12 col-md-6 alert-success">
+                            <div className="intro">
+                            <div className=" col-lg-12 alert-info">
+                                <h2>HTML</h2>
+                            </div>
+                             
+                            
+                                <p>HTML is a markup language that is used for creating web pages. The HTML tutorial section will help you understand the basics of HTML, so that you can create your own web pages.adaddasfsfsfsfsf  sfsffsddfsdfs fsfsfsfsdfsdff</p>
+                            </div>
+                        </div>
+                           <div  className="col-lg-3 col-xs-12  col-md-6 alert-success" >
+                            <div className="intro">
+                            <div className=" col-lg-12 alert-info">
+                                    <h2>CSS</h2>
+                            </div>
+                          
+                                <p>CSS is used for describing the presentation of web pages. The CSS tutorial section will help you learn the essentials of CSS, so that you can fine control the style and layout of your HTML document.</p>
+                           </div>
+                        </div>
+                        <div className=" col-lg-3 col-xs-12 col-md-6 alert-success">
+                            <div className="intro">
+                            <div className=" col-lg-12 alert-info">
+                                <h2>Bootstrap</h2>
+                            </div>
+                                                   
+                                <hr/>
+            
+                                <p>Bootstrap is a powerful front-end framework for faster and easier web development. The Bootstrap tutorial section will help you learn the techniques of Bootstrap so that you can create web your own website.</p>
+                            </div>
+                        </div>
+                        <div className=" col-lg-3 col-xs-12 col-md-6 alert-success" >
+                            <div className="intro">
+                            <div className=" col-lg-12 alert-info">
+                                <h2>Jquery</h2>
+                            </div>
+                                             
+                                <hr/>
+                                
+            
+                                <p>Bootstrap is a powerful front-end framework for faster and easier web development. The Bootstrap tutorial section will help you learn the techniques of Bootstrap so that you can create web your own website.</p>
+                          </div>
+                        </div>
+                    </div>
+            
+                </div>
+            </div>
+        );
+    }
+}
+
